173c6268a9adc9dc27cbc88020f7e6b3757c7194
dannyrgd <danny.ramirezgd@gmail.com>
2022-04-13 19:39:04 -0400

    finished reaction form mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-`
+  }
+`;
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
@@ -22,7 +22,7 @@ export const ADD_USER = gql`
       }
     }
   }
-`
+`;
 
 export const ADD_FRIEND = gql`
   mutation addFriend($id: ID!) {
@@ -36,4 +36,34 @@ export const ADD_FRIEND = gql`
       }
     }
   }
-`
+`;
+
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
